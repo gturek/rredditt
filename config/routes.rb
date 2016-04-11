8eedafb9228bef83ccdd2868d4a0a948eb8e10be
Grzegorz <rubyonrailscoding@gmail.com>
2016-04-11 22:23:30 +0200

    Added and setup acts_as_votable

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  resources :links
+  resources :links do
+  	member do
+  		put "like", 	to: "link#upvote"
+  		put "dislike", 	to: "link#downvote"
+  	end
+  end
 
   root to: "links#index"
 end
